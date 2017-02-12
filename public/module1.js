d4d797fd35fa23a8e4965e5b16b595b612733631
fuadajip <fuadajipratomo@gmail.com>
2017-02-12 16:02:39 +0700

    Modules

---
+++ public/module1.js
@@ -0,0 +1,5 @@
+//Function with return value
+var app = function(fu){
+  return "Hi i\'m module 1 get value from " + fu;
+}
+module.exports = app;
---
+++ public/module2.js
@@ -0,0 +1,2 @@
+var app = require('./module1.js');
+console.log(app('Module2'));
